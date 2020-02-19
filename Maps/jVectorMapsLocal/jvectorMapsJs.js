1ebb9fc60033ed0c5a46131743265a4cfcf08964
Andrea <andcar0302@gmail.com>
2020-02-19 16:27:50 +0100

    aggiunta intestazione sul file locale

--- Maps/jVectorMapsLocal/jvectorMapsJs.js
+++ Maps/jVectorMapsLocal/jvectorMapsJs.js
@@ -1,3 +1,9 @@
 $(function(){
-  $('#map').vectorMap({map: 'it_merc'});
+  $('#map').vectorMap({map: 'it_merc',
+	serie: {
+		regions: [{
+			scale: ['#C8EEFF', '#0071A4'],
+			normalizeFunction: 'polynomial'
+		}]
+	}});
 });
